ad0967afcff23a8a50e6c9d63057c159e4194e57
Arpit Srivastava <arpit.srivastav@appinventiv.com>
2019-12-30 15:49:49 +0530

    commit after project setup

---
+++ CoreDataCURD/CoreDataCURD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataCURD
+//
+//  Created by Arpit Srivastava on 30/12/19.
+//  Copyright © 2019 Arpit Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
